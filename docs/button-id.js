c9942199254d08646ec904f62b35152092ac42cf
khushboo <rajpalkhushbu4@gmail.com>
2025-08-08 15:40:04 +0530

    docs(pages): allow configuring Razorpay Payment Button via docs/button-id.js or ?button= param

---
+++ docs/button-id.js
@@ -0,0 +1,4 @@
+// Set your Razorpay Payment Button ID here to enable the Donate button on GitHub Pages
+// Example: window.RAZORPAY_PAYMENT_BUTTON_ID = 'pl_1234567890abcdef';
+window.RAZORPAY_PAYMENT_BUTTON_ID = '';
+
