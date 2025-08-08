235c63d67864937c76aa0116a71374fc87610abe
khushboo <rajpalkhushbu4@gmail.com>
2025-08-08 13:41:03 +0530

    Initial commit: Django Razorpay integration (config rename, SDK setup)

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+"""Project URL configuration."""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pay.urls')),
+]
